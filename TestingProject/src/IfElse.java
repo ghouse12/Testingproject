f83fbf1f56c116d960237ba2020c80d41aa53780
ghouse <gousaddin.md@gmail.com>
2018-02-10 19:01:48 +0530

    added the java code

---
+++ TestingProject/src/IfElse.java
@@ -0,0 +1,16 @@
+public class IfElse {
+
+	public static void main(String[] args)
+	{
+		int num =70;
+		if (num<70)
+			{
+				System.out.println("number is less than 70");
+			}
+		else
+			{
+			System.out.println("number is greater than 70");
+			}
+
+		}
+	}
